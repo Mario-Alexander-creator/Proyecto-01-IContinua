c6a6b5d4f7a1986cf14b760fec3d4928b778d3ea
Mario Alexander Muñoz Barboza <munozbarbozamarioalexander@gmail.com>
2024-11-14 12:15:26 -0500

    segunda actualizacion

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola Mundo!");
 Console.WriteLine("Mi Nombre es Mario");
+Console.WriteLine("Mi apellido es Mario");
