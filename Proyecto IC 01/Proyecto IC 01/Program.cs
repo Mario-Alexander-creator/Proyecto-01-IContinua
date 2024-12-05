3b874110ca9e91b9c5ffde5b9724e428d15ba070
Mario Alexander Muñoz Barboza <munozbarbozamarioalexander@gmail.com>
2024-12-05 13:01:17 -0500

    clonar-seguridad-semana14-jueves05

--- Proyecto IC 01/Proyecto IC 01/Program.cs
+++ Proyecto IC 01/Proyecto IC 01/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hola Mundo!");
 Console.WriteLine("Mi Nombre es Mario");
 Console.WriteLine("Mi apellido es Mario");
+Console.WriteLine("modificacion semana 14 proyecto jueves 5");
